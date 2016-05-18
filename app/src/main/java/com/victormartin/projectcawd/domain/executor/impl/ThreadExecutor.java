62e258c46be4e5e2849fc5c0c61dcd6637acd768
Juan Jesús Fernández Madrid <jjfernandez@digitalorigin.com>
2016-05-18 13:35:47 +0200

    inject thread executor

--- app/src/main/java/com/victormartin/projectcawd/domain/executor/impl/ThreadExecutor.java
+++ app/src/main/java/com/victormartin/projectcawd/domain/executor/impl/ThreadExecutor.java
@@ -7,25 +7,29 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 /**
  * This singleton class will make sure that each interactor operation gets a background thread.
  * <p/>
  */
+@Singleton
 public class ThreadExecutor implements Executor {
 
     // This is a singleton
     private static volatile ThreadExecutor sThreadExecutor;
 
-    private static final int                     CORE_POOL_SIZE  = 3;
-    private static final int                     MAX_POOL_SIZE   = 5;
-    private static final int                     KEEP_ALIVE_TIME = 120;
-    private static final TimeUnit                TIME_UNIT       = TimeUnit.SECONDS;
-    private static final BlockingQueue<Runnable> WORK_QUEUE      = new LinkedBlockingQueue<Runnable>();
+    private static final int CORE_POOL_SIZE = 3;
+    private static final int MAX_POOL_SIZE = 5;
+    private static final int KEEP_ALIVE_TIME = 120;
+    private static final TimeUnit TIME_UNIT = TimeUnit.SECONDS;
+    private static final BlockingQueue<Runnable> WORK_QUEUE = new LinkedBlockingQueue<Runnable>();
 
     private ThreadPoolExecutor mThreadPoolExecutor;
 
-    private ThreadExecutor() {
+    @Inject
+    public ThreadExecutor() {
         long keepAlive = KEEP_ALIVE_TIME;
         mThreadPoolExecutor = new ThreadPoolExecutor(
                 CORE_POOL_SIZE,
@@ -50,7 +54,8 @@ public class ThreadExecutor implements Executor {
     }
 
     /**
-     * Returns a singleton instance of this executor. If the executor is not initialized then it initializes it and returns
+     * Returns a singleton instance of this executor. If the executor is not initialized then it initializes it and
+     * returns
      * the instance.
      */
     public static Executor getInstance() {
