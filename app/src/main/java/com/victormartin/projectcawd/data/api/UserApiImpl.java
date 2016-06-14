f99155e696947d7426d84608ad092838ac61e053
Juan Jesus Fernandez Madrid <juangefm@gmail.com>
2016-06-14 20:05:22 +0200

    update user api

--- app/src/main/java/com/victormartin/projectcawd/data/api/UserApiImpl.java
+++ app/src/main/java/com/victormartin/projectcawd/data/api/UserApiImpl.java
@@ -9,29 +9,21 @@ import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
 
 public class UserApiImpl implements UserApi {
-
-    private static final String BASE_URL = "www.google.com";
-
-    //TODO: THIS COULD BE EXTRACT TO AN OUTER IFACE
-    public interface UserEndpoint {
-        @POST("/auth/local")
-        Call<User> loginUser(@Body LoginRequestEntity userId);
-    }
 
     @Inject
     public UserApiImpl() { }
 
     @Override
-    public User loginUser() {
+    public User loginUser(String identifier, String password) {
+
         Retrofit retrofitBuilder = getRetrofitBuilder();
-        UserEndpoint userRetrofitApi = retrofitBuilder.create(UserEndpoint.class);
+        UserRetrofitApi userRetrofitApi = retrofitBuilder.create(UserRetrofitApi.class);
 
         LoginRequestEntity loginRequestEntity = new LoginRequestEntity();
-
+        loginRequestEntity.setIdentifier(identifier);
+        loginRequestEntity.setPassword(password);
 
         Call<User> call = userRetrofitApi.loginUser(loginRequestEntity);
         Response<User> response = null;
